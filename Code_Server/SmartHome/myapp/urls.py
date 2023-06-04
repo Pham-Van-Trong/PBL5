a7504d74759f26dc2222e0d267aba0a06c344632
Pham Van trong <phamvantrong10102k2@gmail.com>
2023-06-04 22:04:57 +0700

    cập nhật gọi api của esp8266

--- Code_Server/SmartHome/myapp/urls.py
+++ Code_Server/SmartHome/myapp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import ItemList, ItemDetail, ArduinoDataList, ArduinoDataDetail, update_light_PN_ON, update_light_PN_OFF, update_light_PK_ON, update_light_PK_OFF, update_Door_ON, update_Door_OFF
+from .views import update_light_PN_ON, update_light_PN_OFF, update_light_PK_ON, update_light_PK_OFF, update_Door_ON, update_Door_OFF
 
 urlpatterns = [
-    path('items/', ItemList.as_view()),
-    path('items/<int:pk>/', ItemDetail.as_view()),
-    path('datas/', ArduinoDataList.as_view()),
-    path('datas/<int:pk>/', ArduinoDataDetail.as_view()),
+    # path('items/', ItemList.as_view()),
+    # path('items/<int:pk>/', ItemDetail.as_view()),
+    # path('datas/', ArduinoDataList.as_view()),
+    # path('datas/<int:pk>/', ArduinoDataDetail.as_view()),
 
     path('ledpn_on/', update_light_PN_ON, name='ledpn_on'),
     path('ledpn_off/', update_light_PN_OFF, name='ledpn_off'),
--- Code_Server/SmartHome/myapp/views.py
+++ Code_Server/SmartHome/myapp/views.py
@@ -10,98 +10,57 @@ import requests
 import serial
 import time
 
-
-# Create your views here.
-from rest_framework import generics
-ser = serial.Serial('COM4', 9600)
-
-
-class ItemList(generics.ListCreateAPIView):
-    queryset = Item.objects.all()
-    serializer_class = ItemSerializer
-
-
-class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Item.objects.all()
-    serializer_class = ItemSerializer
-
-
-class ArduinoDataList(generics.ListCreateAPIView):
-    queryset = ArduinoData.objects.all()
-    serializer_class = ArduinoDataSerialezer
-
-
-class ArduinoDataDetail(generics.RetrieveUpdateAPIView):
-    queryset = ArduinoData.objects.all()
-    serializer_class = ArduinoDataSerialezer
-
+esp8266_ip = 'http://192.168.2.36'
 
 def update_light_PN_ON(request):
-    # Lấy trạng thái bật đèn từ cơ sở dữ liệu
-    obj = Item.objects.get(pk=1)
-    obj.status = True
-    obj.save()
-    # Gửi yêu cầu điều khiển đèn tới Arduino
-    # arduino_port = get_arduino_port()
-    data = '1'
-    ser.write(data.encode())
-    return HttpResponse('Light PN On successfully!')
+    url = f'{esp8266_ip}/ledpn/on'
+    response = requests.post(url)
+    if response.status_code == 200:
+        return HttpResponse('Bật đèn phòng ngủ thành công')
+    else:
+        return HttpResponse('Bật đèn phòng ngủ không thành công')
 
 
 def update_light_PN_OFF(request):
-    # Lấy trạng thái bật đèn từ cơ sở dữ liệu
-    obj = Item.objects.get(pk=1)
-    obj.status = False
-    obj.save()
-    # Gửi yêu cầu điều khiển đèn tới Arduino
-    # arduino_port = get_arduino_port()
-    data = '0'
-    ser.write(data.encode())
-    return HttpResponse('Light PN OFF successfully!')
+    url = f'{esp8266_ip}/ledpn/off'
+    response = requests.post(url)
+    if response.status_code == 200:
+        return HttpResponse('Tắt đèn phòng ngủ thành công')
+    else:
+        return HttpResponse('Tắt đèn phòng ngủ không thành công')
 
 
 def update_light_PK_ON(request):
-    # Lấy trạng thái bật đèn từ cơ sở dữ liệu
-    obj = Item.objects.get(pk=5)
-    obj.status = True
-    obj.save()
-    # Gửi yêu cầu điều khiển đèn tới Arduino
-    # arduino_port = get_arduino_port()
-    data = '3'
-    ser.write(data.encode())
-    return HttpResponse('Light PK On successfully!')
+    url = f'{esp8266_ip}/ledpk/on'
+    response = requests.post(url)
+    if response.status_code == 200:
+        return HttpResponse('Bật đèn phòng khách thành công')
+    else:
+        return HttpResponse('Bật đèn phòng khách không thành công')
 
 
 def update_light_PK_OFF(request):
-    obj = Item.objects.get(pk=5)
-    obj.status = False
-    obj.save()
-    # Gửi yêu cầu điều khiển đèn tới Arduino
-    # arduino_port = get_arduino_port()
-    data = '2'
-    ser.write(data.encode())
-    return HttpResponse('Light PK OFF successfully!')
+    url = f'{esp8266_ip}/ledpk/off'
+    response = requests.post(url)
+    if response.status_code == 200:
+        return HttpResponse('Tắt đèn phòng ngủ thành công')
+    else:
+        return HttpResponse('Tắt đèn phòng ngủ không thành công')
 
 
 def update_Door_ON(request):
-    # Cập nhật trạng thái Bật đèn từ cơ sở dữ liệu
-
-    obj = Item.objects.get(pk=4)
-    obj.status = True
-    obj.save()
-    # Gửi yêu cầu điều khiển đèn tới Arduino
-    # arduino_port = get_arduino_port()
-    data = '4'
-    ser.write(data.encode())
-    return HttpResponse('Door ON successfully!')
+    url = f'{esp8266_ip}/door/on'
+    response = requests.post(url)
+    if response.status_code == 200:
+        return HttpResponse('Mở cửa thành công')
+    else:
+        return HttpResponse('Mở cửa không thành công')
 
 
 def update_Door_OFF(request):
-    # Cập nhật trạng thái tắt đèn từ cơ sở dữ liệu
-    obj = Item.objects.get(pk=4)
-    obj.status = False
-    obj.save()
-    data = '5'
-    ser.write(data.encode())
-    return HttpResponse('Door OFF successfully!')
-
+    url = f'{esp8266_ip}/door/off'
+    response = requests.post(url)
+    if response.status_code == 200:
+        return HttpResponse('Đóng cửa thành công')
+    else:
+        return HttpResponse('Đóng cửa không thành công')
