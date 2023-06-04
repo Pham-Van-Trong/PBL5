dbfed7aed45b2704a6ec15f848b31eb9587e7e37
Pham Van trong <phamvantrong10102k2@gmail.com>
2023-06-04 22:10:39 +0700

    cập nhật hàm điều khiển thiết bị trên esp8266

--- Code_Arduino/pbl5.ino
+++ Code_Arduino/pbl5.ino
@@ -1,38 +1,79 @@
-#include<Servo.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <Servo.h>
 Servo myservo;
-int gas = 0;
+
+ESP8266WebServer server(80);
+
+int gas = A0;
 int val = 0;
-int ledPN = 12;
-int ledPK = 13;
-int loa = 10;
-void setup() {
-  Serial.begin(9600);
-  pinMode(ledPN,OUTPUT);
-  pinMode(ledPK,OUTPUT);
-  pinMode(loa,OUTPUT);
-  myservo.attach(11);
+int ledPN = D0;
+int ledPK = D1;
+int loa = D2;
+
+void ledpnon()
+{
+    digitalWrite(ledPN, HIGH);
+    server.send(200, "text/plain", "Success");
+}
+void ledpnoff()
+{
+    digitalWrite(ledPN, LOW);
+    server.send(200, "text/plain", "Success");
+}
+void ledpkon()
+{
+    digitalWrite(ledPK, HIGH);
+    server.send(200, "text/plain", "Success");
+}
+void ledpkoff()
+{
+    digitalWrite(ledPK, LOW);
+    server.send(200, "text/plain", "Success");
+}
+void dooron()
+{
+    myservo.write(180);
+    server.send(200, "text/plain", "Success");
 }
 
-void loop() {
-  if (Serial.available() > 0) {
-    int state = Serial.read() - '0';
-    switch(state) {
-      case 0: digitalWrite(ledPN,LOW);
-              break;
-      case 1: digitalWrite(ledPN, HIGH);
-              break;
-      case 2: digitalWrite(ledPK, LOW);
-              break;
-      case 3: digitalWrite(ledPK, HIGH);
-              break;
-      case 4: myservo.write(90);
-              break;
-      case 5: myservo.write(0);
-              break;
+void dooroff()
+{
+    myservo.write(0);
+    server.send(200, "text/plain", "Success");
+}
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(ledPN, OUTPUT);
+    pinMode(ledPK, OUTPUT);
+    pinMode(loa, OUTPUT);
+    myservo.attach(D3);
+    WiFi.begin("Wifi Chua", "38383838@");
+    while (WiFi.waitForConnectResult() != WL_CONNECTED)
+    {
+        Serial.print(".");
     }
-  }
-  val = analogRead(gas);
-  Serial.println(val, DEC);
-  if(val > 200) digitalWrite(loa,HIGH);
-  else digitalWrite(loa,LOW);
+    Serial.println("IP: ");
+    Serial.print(WiFi.localIP());
+
+    server.on("/ledpn/on", ledpnon);
+    server.on("/ledpn/off", ledpnoff);
+    server.on("/ledpk/on", ledpkon);
+    server.on("/ledpk/off", ledpkoff);
+    server.on("/door/on", dooron);
+    server.on("/door/off", dooroff);
+    server.begin();
+}
+
+void loop()
+{
+    server.handleClient();
+    val = analogRead(gas);
+    Serial.println(val, DEC);
+    if (val > 500)
+        digitalWrite(loa, HIGH);
+    else
+        digitalWrite(loa, LOW);
 }
